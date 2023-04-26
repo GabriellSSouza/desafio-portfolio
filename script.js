b94b3c1e1a3066516dbb582fd4e85d7c2ad618b6
Gabriel Souza <gabrieldesouza003@gmail.com>
2023-04-26 15:02:47 -0300

    add dados dos respositorios

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const foto_perfil = document.querySelector('.foto_perfil')
 const nome_perfil = document.querySelector('.nome_perfil')
 const sociais_localizacao = document.querySelector('.redes_socias_localizacao')
+const login_github = document.querySelector('.login_github')
+const projeto1_nome = document.querySelector('.projeto1_nome')
+const projeto1_desc = document.querySelector('.projeto1_desc')
+const projeto2_nome = document.querySelector('.projeto2_nome')
+const projeto2_desc = document.querySelector('.projeto2_desc')
 
-//login_gihub
-
+//github - forks stargazers_count classes - projeto1_stars projeto1_forks
 const url = "https://api.github.com/users/gabriellssouza"
 
 fetch(url)
@@ -13,5 +17,17 @@ fetch(url)
         foto_perfil.src = data.avatar_url
         nome_perfil.innerHTML = data.name
         sociais_localizacao.childNodes[2].textContent = data.location
+        login_github.childNodes[2].textContent = data.login
+        fetch(data.repos_url)
+            .then(response =>response.json())
+            .then(data =>{
+                console.log(data)
+                projeto1_nome.textContent = data[5].name
+                projeto1_desc.textContent = data[5].description
+                projeto2_nome.textContent = data[8].name
+                projeto2_desc.textContent = data[8].description
+
+            })
+        
     })
     .catch(error => console.error(error))
